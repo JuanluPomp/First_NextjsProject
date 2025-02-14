8e36007459c8ff4ea9e2c508013a8b55dc16e162
Juan Lizarraga Dev <juanluislizarragapompa3@gmail.com>
2025-02-14 14:51:12 -0700

    Primer commit

---
+++ app/page.jsx
@@ -0,0 +1,28 @@
+import UsersCard from '@/components/UsersCard'
+import React from 'react'
+
+export async function FetchUsers(){{
+    const res = await fetch('https://reqres.in/api/users?page=2')
+    const {data} = await res.json()
+    return data
+}}
+
+export default async function HomePage() {
+  const data = await FetchUsers()
+  console.log(data)
+  return (
+    <>
+      <h1 className=' text-center font-bold text-3xl mt-10'>Usuarios</h1>
+      <div className=' grid grid-cols-2 lg:grid-cols-3 m-10 gap-5'>
+          {data.map(user => (
+            <UsersCard
+            key={user.id}
+              user={user}
+            />
+          ))}
+      </div>
+      
+      
+    </>
+  )
+}
---
+++ components/UsersCard.jsx
@@ -0,0 +1,14 @@
+
+export default function UsersCard({user}) {
+  return (
+    <div className=" max-w-90 grid grid-cols-2 gap-2 space-y-5 mt-10 p-4 border border-slate-300 shadow-lg bg-white">
+        <div className=" max-w-30 ">
+            <img className="rounded-full mx-auto p-1 w-full" src={user.avatar} alt="" />
+        </div>
+        <div className=" text-xl font-bold text-left space-y-5">
+            <p>Nombre: <span className=" font-normal">{user.first_name} {''} {user.last_name}</span></p>
+            <p>Email: <span className=" font-normal">{user.email}</span></p>
+        </div>
+    </div>
+  )
+}
